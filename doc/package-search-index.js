5eac758caa6d2b5c6d58df75950e73314836f0f0
GabiHERON <heron060907@gmail.com>
2026-06-13 23:15:31 -0300

    Add files via upload

---
+++ doc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html","k":"18"},{"l":"Eventos"},{"l":"Exceptions"},{"l":"Geral"},{"l":"Inimigos"},{"l":"Jogador"}];updateSearchResults();
